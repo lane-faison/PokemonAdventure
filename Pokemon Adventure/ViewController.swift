dae57b53852465ec74a07cd02cc8d9359c35864c
Lane Faison <lane.faison@icloud.com>
2017-05-06 15:00:53 -0600

    Map centers on user

--- Pokemon Adventure/ViewController.swift
+++ Pokemon Adventure/ViewController.swift
@@ -7,19 +7,43 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var manager = CLLocationManager() // This is used to ask user for permission to use location
+    
+    var updateCount = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        manager.delegate = self
+        
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+            print("Ready to go!")
+            mapView.showsUserLocation = true
+            manager.startUpdatingLocation()
+        } else {
+            manager.requestWhenInUseAuthorization()
+        }
+        
+        manager.requestWhenInUseAuthorization()
+        
+        mapView.showsUserLocation = true
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if updateCount < 3 {
+            let region = MKCoordinateRegionMakeWithDistance(manager.location!.coordinate, 1000, 1000)
+            mapView.setRegion(region, animated: false) // Put true if you want to see maps zooming in
+            updateCount += 1
+        }
     }
-
-
+    
 }
 
