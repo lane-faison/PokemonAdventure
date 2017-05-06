ab37d2e5e9bec9e769a900e61ac1eecbcb21c165
Lane Faison <lane.faison@icloud.com>
2017-05-06 14:21:49 -0600

    Initial Commit

---
+++ Pokemon Adventure/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pokemon Adventure
+//
+//  Created by Lane Faison on 5/6/17.
+//  Copyright © 2017 RunningSocial. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
